dfec7ef2884475867c78b447b22325604ad4baaf
Amir <amir@example.com>
2025-12-14 04:35:42 +0300

    fix: add Netlify domain to CORS and prod config

--- app/main.py
+++ app/main.py
@@ -30,7 +30,7 @@ app.add_middleware(
          # Netlify frontend
         "https://sparkly-choux-12aad6.netlify.app",
 
-         "https://front-1kl2wk708-amirs-projects-d2eb9917.vercel.app"
+         
           "https://fitness-tracker-cyan-three.vercel.app"
         
        
